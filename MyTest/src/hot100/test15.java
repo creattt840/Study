8b543980b47a1574ed1ca51ac894e1bbbca78569
xu <2813519505@qq.com>
2026-05-21 11:58:09 +0800

    力扣hot100

---
+++ MyTest/src/hot100/test15.java
@@ -0,0 +1,33 @@
+package hot100;
+
+public class test15 {
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+
+         TreeNode() {
+         }
+
+         TreeNode(int val) {
+             this.val = val;
+         }
+
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
+    public int maxDepth(TreeNode root) {
+        if (root==null) {
+            return 0;
+        }else {
+            int leftHeight=maxDepth(root.left);
+            int rightHeight=maxDepth(root.right);
+            return Math.max(leftHeight,rightHeight)+1;
+        }
+    }
+
+}
