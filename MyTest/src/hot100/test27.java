7eb43faebaf6e8f8f0105c2bbd0ae4213dbb0774
054 <2813519505@qq.com>
2026-07-27 17:18:54 +0800

    hot100

---
+++ MyTest/src/hot100/test27.java
@@ -0,0 +1,20 @@
+package hot100;
+
+public class test27 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
